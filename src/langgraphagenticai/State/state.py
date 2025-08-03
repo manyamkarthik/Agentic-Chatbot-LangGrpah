df214432e5f356f83dab76bd7b588efb4daf60ed
Karthik Manyam <harikarthikmanyam@gmail.com>
2025-08-03 14:20:57 -0400

    Basic ChatBot Implementation

---
+++ src/langgraphagenticai/State/state.py
@@ -0,0 +1,5 @@
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated
+class State(TypedDict):
+    messages: Annotated[list,add_messages]
